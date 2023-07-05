6a2db3065bf9b8de76b31b798c095b66f79b1d1e
Nastia Karpova <akkarpova@gmail.com>
2023-07-05 20:22:15 +0200

    Add return values for playRound function, for later use

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 console.log("Hello there! Welcome to a little game..");
 
-const playerSelection = "rock";
+const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
-    const handSignals = ["rock", "paper", "scissors"];
+    const handSignals = ["Rock", "Paper", "Scissors"];
     const chosenSignal = Math.floor(Math.random() * 3);
     return handSignals[chosenSignal];
 }
 
 function playRound(playerSelection, computerSelection) {
+
+    const youWin = `You win! ${playerSelection} beats ${computerSelection}.`;
+    const youLose = `You lose! ${computerSelection} beats ${playerSelection}.`;
+    const itIsTie = `Its a tie! Try again.`;
+
+
     if (playerSelection === computerSelection) {
-        console.log(`Its a tie! Try again.`);
+        return itIsTie;
     }
 
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        return youWin;
     }
 
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        return youWin;
     }
 
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        return youWin;
     }
 
     else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        return youLose;
     }
 
 }
@@ -35,19 +41,19 @@ function playRound(playerSelection, computerSelection) {
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
-console.log(playRoundAlternative(playerSelection, computerSelection));
+// console.log(playRoundAlternative(playerSelection, computerSelection));
 
 
 
-function playRoundAlternative(playerSelection, computerSelection) {
+// function playRoundAlternative(playerSelection, computerSelection) {
 
-    const youWin = `You win! ${playerSelection} beats ${computerSelection}.`;
-    const youLose = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    const itIsTie = `Its a tie! Try again.`;
+//     const youWin = `You win! ${playerSelection} beats ${computerSelection}.`;
+//     const youLose = `You lose! ${computerSelection} beats ${playerSelection}.`;
+//     const itIsTie = `Its a tie! Try again.`;
 
-    return (playerSelection === computerSelection) ? itIsTie
-        : (playerSelection === "paper" && computerSelection === "rock") ? youWin
-        : (playerSelection === "rock" && computerSelection === "scissors") ? youWin
-        : (playerSelection === "scissors" && computerSelection === "paper") ? youWin
-        : youLose;
-}
+//     return (playerSelection === computerSelection) ? itIsTie
+//         : (playerSelection === "paper" && computerSelection === "rock") ? youWin
+//         : (playerSelection === "rock" && computerSelection === "scissors") ? youWin
+//         : (playerSelection === "scissors" && computerSelection === "paper") ? youWin
+//         : youLose;
+// }
